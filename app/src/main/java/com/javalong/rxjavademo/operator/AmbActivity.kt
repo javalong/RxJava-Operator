095da3dc9239e6d71fd6dd9d2a7372c9a687e5cc
lingo <linghu@dian.so>
2018-04-16 13:35:42 +0800

    no message

--- app/src/main/java/com/javalong/rxjavademo/operator/AmbActivity.kt
+++ app/src/main/java/com/javalong/rxjavademo/operator/AmbActivity.kt
@@ -1,12 +1,15 @@
 package com.javalong.rxjavademo.operator
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import com.javalong.rxjavademo.R
 import kotlinx.android.synthetic.main.activity_common.*
 import rx.Observable
+import rx.Scheduler
 import rx.Subscriber
 import rx.Subscription
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by 令狐 on 18/4/15.
@@ -18,19 +21,33 @@ class AmbActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_common)
 
+//        observable = Observable.amb(Observable.create(object : Observable.OnSubscribe<String> {
+//            override fun call(t: Subscriber<in String>) {
+//                t.onNext("Test1")
+//            }
+//        }), Observable.create(object : Observable.OnSubscribe<String> {
+//            override fun call(t: Subscriber<in String>) {
+//                t.onNext("Test2")
+//            }
+//        }))
+        var handler = Handler()
         observable = Observable.amb(Observable.create(object : Observable.OnSubscribe<String> {
             override fun call(t: Subscriber<in String>) {
                 t.onNext("Test1")
             }
-        }), Observable.create(object : Observable.OnSubscribe<String> {
+        }).delay(1100, TimeUnit.SECONDS), Observable.create(object : Observable.OnSubscribe<String> {
             override fun call(t: Subscriber<in String>) {
+
                 t.onNext("Test2")
             }
-        }))
+        }).delay(1100, TimeUnit.SECONDS))
+
 
         btSub.setOnClickListener({
             observable?.subscribe({ msg ->
-                tvContent.text = tvContent.text.toString() + "\n" + msg
+                handler.post({
+                    tvContent.text = tvContent.text.toString() + "\n" + msg
+                })
             })
         })
 
